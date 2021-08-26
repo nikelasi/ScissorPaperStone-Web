04483dea97ae9adef5bf21f7f2cd4b565307760f
NicholasJohansan <nicholasjohansan@gmail.com>
2021-08-26 16:04:25 +0800

    added functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import ResultCard from './ResultCard';
+import { useState } from 'react'
 
 function App() {
+
+  const [computerChose, setComputerChose] = useState('???')
+  const [userChose, setUserChose] = useState('???')
+  const [result, setResult] = useState('???')
+
+  const evaluateOption = (option: string) => {
+    const options = ["Scissor", "Paper", "Stone"]
+    setComputerChose(options[Math.floor(Math.random() * options.length)])
+    setUserChose(option)
+    if (
+      (userChose === "Scissor" && computerChose === "Paper") || 
+      (userChose === "Paper" && computerChose === "Stone") ||
+      (userChose === "Stone" && computerChose === "Scissor")
+    ) { setResult("You won!") }
+    else if (userChose === computerChose) { setResult("Tie!") }
+    else if (
+      (userChose === "Scissor" && computerChose === "Stone") ||
+      (userChose === "Paper" && computerChose === "Scissor") ||
+      (userChose === "Stone" && computerChose === "Paper")
+    ) { setResult("Computer won!") }
+  }
+
   return (
     <div className="App">
       <div className="content">
         <h1>Scissor Paper Stone</h1>
         <div className="row" id="actions">
           <div className="col-xs-4">
-            <button className="col-xs-12 shadow" id="scissor"><img className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/scissor.svg"}/></button>
+            <button className="col-xs-12 shadow" onClick={ () => evaluateOption('Scissor') }>
+              <img alt="Scissor" className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/scissor.svg"}/>
+            </button>
           </div>
           <div className="col-xs-4">
-            <button className="col-xs-12 shadow" id="paper"><img className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/paper.svg"}/></button>
+            <button className="col-xs-12 shadow" onClick={ () => evaluateOption('Paper') }>
+              <img alt="Paper" className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/paper.svg"}/>
+            </button>
           </div>
           <div className="col-xs-4">
-            <button className="col-xs-12 shadow" id="stone"><img className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/stone.svg"}/></button>
+            <button className="col-xs-12 shadow" onClick={ () => evaluateOption('Stone') }>
+              <img alt="Stone" className="col-xs-12 img-fluid" src={process.env.PUBLIC_URL + "/imgs/stone.svg"}/>
+            </button>
           </div>
         </div>
-        <ResultCard />
+        <ResultCard computerChose={computerChose} userChose={userChose} result={result}/>
       </div>
     </div>
+  );
+}
+
+export interface ResultCardProps {
+  computerChose: string
+  userChose: string
+  result: string
+}
+
+const ResultCard: React.FunctionComponent<ResultCardProps> = ({ computerChose, userChose, result }) => {
+  return (
+      <div id="result-card" className="col-xs-12 shadow">
+          <p id="result">
+              Computer Chose: { computerChose }<br/>
+              You Chose: { userChose }<br/>
+              Result: { result }
+          </p>
+      </div>
   );
 }
 
